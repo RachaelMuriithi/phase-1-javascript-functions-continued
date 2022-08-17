9d3c1f9d0f1b3d723e6ffec2052ab6ce59646d64
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-17 17:53:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return 'This Saturday, I want to ' + activity + '!';
+}
+saturdayFun ( ' bathe my dog');
+
+function mondayWork ( activity = 'go to the office') {
+    return 'This Monday, I will ' + activity + '.'
+}
+mondayWork ('work from home')
+
+function wrapAdjective (gift) {
+    if (gift === '*') {
+        return function (today = 'special') {
+            return `You are *${today}*!`
+    }
+    
+    } else if (gift === '||') {
+        return function (fine) {
+            return `You are ||${fine}||!`
+        }
+    }
+    
+}
+wrapAdjective ('*')('a hard worker')
+wrapAdjective ('||')('a dedicated programmer')
